0f67d8854cebedcffc223cef6502e48c0d536b34
bou1337 <aitbougnsa.ismail@gmail.com>
2025-04-13 18:43:15 +0100

    makefile adjust

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ FLAGS =-Wall -Wextra -Werror
 MLX = -lmlx -lXext -lX11 -lm
 
 SRCS = main.c  init.c intr.c  player.c util.c cast_ray.c draw.c exit.c handel_key.c draw_map.c parser/parser1.c parser/parser2.c parser/parser3.c parser/parser4.c parser/parser5.c parser/parser6.c \
-		parser/parser7.c parser/parser8.c parser/parser9.c parser/paser10.c parser/parser11.c
+		parser/parser7.c parser/parser8.c parser/parser9.c parser/paser10.c parser/parser11.c check.c
 OBJS = $(SRCS:.c=.o)
 
 all: $(NAME)
--- parser/parser2.c
+++ parser/parser2.c
@@ -27,62 +27,62 @@ int	parse_int(const char *str, int *i)
     return (sign * num);
 }
 
- int	check_color_component(char *line, int *i, int *component)
-{
-    int	j;
+//  int	check_color_component(char *line, int *i, int *component)
+// {
+//     int	j;
 
-    j = *i;
-    *component = parse_int(line, i);
-    if (*component < 0 || *component > 255 || *i == j)
-        return (0);
-    return (1);
-}
+//     j = *i;
+//     *component = parse_int(line, i);
+//     if (*component < 0 || *component > 255 || *i == j)
+//         return (0);
+//     return (1);
+// }
 
-int	parse_color(char *line, t_data *data)
-{
-    int	i;
-    int	components_found;
+// int	parse_color(char *line, t_data *data)
+// {
+//     int	i;
+//     int	components_found;
     
-    i = 1;
-    components_found = 0;
-    while (line[i] == ' ' || line[i] == '\t')
-        i++;
-    if (!check_color_component(line, &i, &data->r))
-        return (0);
-    components_found++;
-    if (line[i++] != ',')
-        return (0);
-    if (!check_color_component(line, &i, &data->g))
-        return (0);
-    components_found++;
-    if (line[i++] != ',')
-        return (0);
-    if (!check_color_component(line, &i, &data->b))
-        return (0);
-    components_found++;
-    while (line[i] == ' ' || line[i] == '\t')
-        i++;
-    return (line[i] == '\0' && components_found == 3);
-}
+//     i = 1;
+//     components_found = 0;
+//     while (line[i] == ' ' || line[i] == '\t')
+//         i++;
+//     if (!check_color_component(line, &i, &data->r))
+//         return (0);
+//     components_found++;
+//     if (line[i++] != ',')
+//         return (0);
+//     if (!check_color_component(line, &i, &data->g))
+//         return (0);
+//     components_found++;
+//     if (line[i++] != ',')
+//         return (0);
+//     if (!check_color_component(line, &i, &data->b))
+//         return (0);
+//     components_found++;
+//     while (line[i] == ' ' || line[i] == '\t')
+//         i++;
+//     return (line[i] == '\0' && components_found == 3);
+// }
 
-int	is_direction_identifier(char c1, char c2)
-{
-    return ((c1 == 'N' && c2 == 'O') || (c1 == 'S' && c2 == 'O') ||
-        (c1 == 'W' && c2 == 'E') || (c1 == 'E' && c2 == 'A'));
-}
+// int	is_direction_identifier(char c1, char c2)
+// {
+//     return ((c1 == 'N' && c2 == 'O') || (c1 == 'S' && c2 == 'O') ||
+//         (c1 == 'W' && c2 == 'E') || (c1 == 'E' && c2 == 'A'));
+// }
 
-char	*allocate_path(char *line, int start)
-{
-    int		j;
-    int		len;
-    char	*path;
+// char	*allocate_path(char *line, int start)
+// {
+//     int		j;
+//     int		len;
+//     char	*path;
 
-    j = start;
-    while (line[j] && line[j] != ' ' && line[j] != '\t')
-        j++;
-    len = j - start;
-    path = malloc(sizeof(char) * (len + 1));
-    if (!path)
-        return (NULL);
-    return (path);
-}
+//     j = start;
+//     while (line[j] && line[j] != ' ' && line[j] != '\t')
+//         j++;
+//     len = j - start;
+//     path = malloc(sizeof(char) * (len + 1));
+//     if (!path)
+//         return (NULL);
+//     return (path);
+// }
